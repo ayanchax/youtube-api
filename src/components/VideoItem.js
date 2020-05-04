27b0b49ab7edf23022c7553ff2b40d7f4dd032ec
Swaroop <swaroop.chakraborty@cerner.com>
2020-05-04 10:55:16 +0530

    commit

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({ video, onVideoSelect }) =>{
     return(
         <Grid item xs={12}>
 
-            <Paper style={{display:'flex',alignItems:'center',cursor: 'pointer'}}>
+            <Paper onClick={()=>onVideoSelect(video)} style={{display:'flex',alignItems:'center',cursor: 'pointer'}}>
 <img style={{marginRight: '20px', cursor: 'pointer'}} alt={video.snippet.title} onClick={()=>onVideoSelect(video)} src ={video.snippet.thumbnails.medium.url}/>
 <Typography variant='subtitle1'><b>{video.snippet.title}</b></Typography>
             </Paper>
